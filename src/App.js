6472cfb33b18e6a2008626f440108db81f04efd5
celestinebonaert <celestine.bonaert@gmail.com>
2018-11-07 13:56:39 +0100

    Simpson atome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Quotes from "./Quotes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true
+    };
+  }
+  handleClick = () => {
+      this.setState({ working: !this.state.working });
+    };
   render() {
+    const light = this.state.working ? 'PAUSE' : 'WORKING';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={this.state.working?"App-logo":"App-logo2"} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+          <div className="App">
+        <button onClick={this.handleClick} className={light}>{light.toUpperCase()}</button>
+        <figure className={light} />
+      </div>
         </header>
+        <Quotes/>
+        
       </div>
     );
   }
